ffa6747c9d0bbc1b0cf6375c2238a97d2b176717
Aleksandar Urosevic <alek.sandar0@yandex.com>
2024-01-24 14:44:16 +0100

    Add image cropping script.

---
+++ Tool/Crop.cmd
@@ -0,0 +1,5 @@
+cd ..\Document\Image\Snapshot
+& "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert GeneticProgramming1.png -crop 1533x891+174+68 .\GeneticProgramming1.png
+& "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert GeneticProgramming4.png -crop 1533x891+174+68 .\GeneticProgramming4.png
+& "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert Bruteforce1.png         -crop 1533x891+174+68 .\Bruteforce1.png
+& "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert Bruteforce4.png         -crop 1533x891+174+68 .\Bruteforce4.png
