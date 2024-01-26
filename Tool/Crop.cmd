49f3cb2b32c4ef945188993c8afe726232ec8d83
Aleksandar Urosevic <alek.sandar0@yandex.com>
2024-01-26 02:13:23 +0100

    Dodatne komande za isecanje slika.

--- Tool/Crop.cmd
+++ Tool/Crop.cmd
@@ -1,5 +1,6 @@
 cd ..\Document\Image\Snapshot
 & "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert GeneticProgramming1.png -crop 1533x891+174+68 .\GeneticProgramming1.png
 & "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert GeneticProgramming4.png -crop 1533x891+174+68 .\GeneticProgramming4.png
+& "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert GeneticProgramming5.png -crop 1533x891+174+68 .\GeneticProgramming5.png
 & "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert Bruteforce1.png         -crop 1533x891+174+68 .\Bruteforce1.png
 & "C:\Program Files\Autodesk\Maya2024\bin\magick.exe" convert Bruteforce4.png         -crop 1533x891+174+68 .\Bruteforce4.png
